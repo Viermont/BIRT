b2ab0e09a3689cdc7a5b44ac1af55e87e7109140
Javier Montero <85358456+Viermont@users.noreply.github.com>
2022-10-10 19:37:00 +0200

    Rename Programming/Cohete.java to Programming/PROG01_02/Cohete.java

--- Programming/PROG01_02/Cohete.java
+++ Programming/PROG01_02/Cohete.java
@@ -1,31 +1,31 @@
 /*******************************************************************************
 
    *Autor: Javier Montero Martinez                          *Fecha: 13/10/2021   
-   *Módulo: Programación         *Unidad: 01       *Tarea: Tarea Evaluación 02
+   *MÃ³dulo: ProgramaciÃ³n         *Unidad: 01       *Tarea: Tarea EvaluaciÃ³n 02
    
-   *Descripción: Realiza un programa en Java. Programa que visualiza por
-   consola una figura simétrica compleja realizada mediante bucles for.
+   *DescripciÃ³n: Realiza un programa en Java. Programa que visualiza por
+   consola una figura simÃ©trica compleja realizada mediante bucles for.
    
-   *Enlace autoevaluación:
+   *Enlace autoevaluaciÃ³n:
    https://drive.google.com/drive/folders/16Z0sSfm1nIqq5Y7e4lgQbj9tES6xOLpr?usp=sharing
 
 *******************************************************************************/
 public class Cohete {
-/*El método main realiza las siguientes acciones:
-   1- Escribe el texto solicitado seguido de un salto de línea
-   2- Escribe una línea en blanco seguido de salto de línea
-   3- Llama al método dibujo para que se ejecute*/
+/*El mÃ©todo main realiza las siguientes acciones:
+   1- Escribe el texto solicitado seguido de un salto de lÃ­nea
+   2- Escribe una lÃ­nea en blanco seguido de salto de lÃ­nea
+   3- Llama al mÃ©todo dibujo para que se ejecute*/
    public static void main (String[] args) {
       System.out.println("DIBUJA ESTA FIGURA:");
       System.out.println();
       dibujo();
    }
-/*El método dibujo realiza varias acciones:
-   1- Llama en bucle 4 veces al método ejeCentral
-   2- Llama al método CuerpoSuperior
-   3- Llama al método CuerpoInferior
-   4- Llama en bucle 12 veces al método ejeCentral
-   5- Llama al método CuerpoSuperior
+/*El mÃ©todo dibujo realiza varias acciones:
+   1- Llama en bucle 4 veces al mÃ©todo ejeCentral
+   2- Llama al mÃ©todo CuerpoSuperior
+   3- Llama al mÃ©todo CuerpoInferior
+   4- Llama en bucle 12 veces al mÃ©todo ejeCentral
+   5- Llama al mÃ©todo CuerpoSuperior
 */
    public static void dibujo() {
       for(int linea = 1; linea <= 4; linea++) {
@@ -38,16 +38,16 @@ public class Cohete {
       }
       cuerpoSuperior();
    }
-//El método barraVertical dibuja una doble barra vertical
+//El mÃ©todo barraVertical dibuja una doble barra vertical
    public static void barraVertical() {
       System.out.print("||");
    }
-//El método saltoLinea imprime un salto de línea
+//El mÃ©todo saltoLinea imprime un salto de lÃ­nea
    public static void saltoLinea() {
       System.out.println();
    }
-/*El método ejeCentral imprime con un bucle 12 espacios en blanco, después
-llama al método barraVertical y al método saltoLinea*/
+/*El mÃ©todo ejeCentral imprime con un bucle 12 espacios en blanco, despuÃ©s
+llama al mÃ©todo barraVertical y al mÃ©todo saltoLinea*/
    public static void ejeCentral() {
       for(int espacio = 1; espacio <= 12; espacio++) {
          System.out.print(" ");
@@ -55,14 +55,14 @@ llama al método barraVertical y al método saltoLinea*/
       barraVertical();
       saltoLinea();
    }
-/*El método cuerpoSuperior realiza en bucle 4 veces lo siguiente:
+/*El mÃ©todo cuerpoSuperior realiza en bucle 4 veces lo siguiente:
    1- Dibuja (-3 * linea + 12) espacios en blanco
    2- Dibuja __/
    3- Dibuja (3 * linea - 3) :
-   4- Llama al método barraVertical
+   4- Llama al mÃ©todo barraVertical
    5- Dibuja (3 * linea - 3) :
-   6- Dibuja \__ seguido de salto de línea
-Una vez terminado el bucle llama al método cuerpoMedio*/
+   6- Dibuja \__ seguido de salto de lÃ­nea
+Una vez terminado el bucle llama al mÃ©todo cuerpoMedio*/
    public static void cuerpoSuperior() {
       for(int linea = 1; linea <= 4; linea++) {
          for(int i = 1; i <= -3 * linea + 12; i++) {
@@ -80,10 +80,10 @@ Una vez terminado el bucle llama al método cuerpoMedio*/
       }
       cuerpoMedio();
    }
-/*El método cuerpoMedio realiza las siguientes acciones:
+/*El mÃ©todo cuerpoMedio realiza las siguientes acciones:
    1- Dibuja |
    2- Realiza un bucle que dibuja 24 veces "
-   3- Dibuja | seguida de salto de línea*/
+   3- Dibuja | seguida de salto de lÃ­nea*/
    public static void cuerpoMedio() {
       System.out.print("|");
       for(int i = 1; i <= 24; i++){
@@ -91,11 +91,11 @@ Una vez terminado el bucle llama al método cuerpoMedio*/
       }
       System.out.println("|");
    }
-/*El método cuerpoInferior realiza en bucle 4 veces lo siguiente:
+/*El mÃ©todo cuerpoInferior realiza en bucle 4 veces lo siguiente:
    1- Dibuja (2 * linea - 2) espacios en blanco
    2- Dibuja \_
    3- Dibuja (-2 * linea + 13) /\
-   4- Dibuja _/ seguido de salto de línea*/
+   4- Dibuja _/ seguido de salto de lÃ­nea*/
    public static void cuerpoInferior() {
       for(int linea = 1; linea <= 4; linea++) {
          for(int i = 1; i <= 2 * linea - 2; i++) {
